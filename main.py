dd7eced8b20604a46b20b60bfe1e44a8e76b4710
Venusstars1 <nkohavi2028@kehillahstudent.org>
2025-01-13 08:44:38 -0800

    finished

---
+++ main.py
@@ -0,0 +1,12 @@
+
+def alternate_letters():
+    word = input("Please input any sentence you would like me to alternate caps for you! ")
+    word = word.replace("a", "A")
+    word = word.replace("e", "E")
+    word = word.replace("o", "O")
+    word = word.replace("u", "U")
+    word = word.replace("i", "I")
+    print(word)
+    return
+alternate_letters()
+    
